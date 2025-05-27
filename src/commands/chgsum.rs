3277bc58bcf861eb76e8f1da0849d0669e467171
William Laderer <wtladerer@gmail.com>
2025-05-26 19:29:01 -0700

    better implementation

--- src/commands/chgsum.rs
+++ src/commands/chgsum.rs
@@ -1,58 +1,56 @@
 use std::path::PathBuf;
-
-use anyhow::{Context, Result};
 use clap::Args;
 use log::info;
+use anyhow::{anyhow, Context};
 use rayon::prelude::*;
-
 use crate::{
+    types::Result,
     ChargeDensity,
     ChargeType,
     OptProcess,
 };
 
 #[derive(Debug, Args)]
-/// Sum multiple CHGCAR files to generate a combined charge density.
-/// The operation is: `chgcar_sum = chgcar_1 + chgcar_2 + ... + chgcar_n`
+/// Calculate charge density sum from multiple CHGCAR files.
+///
+/// All CHGCARs must have the same grid and lattice.
 pub struct Chgsum {
-    /// The input CHGCAR files
+    /// Input CHGCAR files to sum
     #[arg(required = true)]
     input: Vec<PathBuf>,
 
-    /// The output CHGCAR file path
+    /// Output file name (default: CHGSUM.vasp)
     #[arg(short, long, default_value = "CHGSUM.vasp")]
     output: PathBuf,
 }
 
 impl OptProcess for Chgsum {
     fn process(&self) -> Result<()> {
-        if self.input.is_empty() {
-            anyhow::bail!("No CHGCAR files provided for summation.");
+        if self.input.len() < 2 {
+            return Err(anyhow!("Please provide at least two CHGCAR files."));
         }
 
-        info!("Reading {} charge densities in parallel...", self.input.len());
-
-        let charges: Vec<ChargeDensity> = self
-            .input
+        // Load all CHGCARs in parallel
+        let chgcars: Result<Vec<ChargeDensity>> = self.input
             .par_iter()
             .map(|path| {
+                info!("Reading charge density from {:?}", path);
                 ChargeDensity::from_file(path, ChargeType::Chgcar)
-                    .with_context(|| format!("Failed to read CHGCAR from {:?}", path))
+                    .with_context(|| format!("Failed to read charge density from {:?}", path))
             })
-            .collect::<Result<_>>()?;
+            .collect();
 
-        info!("Successfully read all CHGCAR files. Summing...");
+        let chgcars = chgcars?;
 
-        // Reduce the vector with addition
-        let mut chg_sum = charges[0].clone();
-        for chg in &charges[1..] {
-            chg_sum = (chg_sum.clone() + chg.clone())?;
-        }
+        // Reduce all ChargeDensity files into one
+        let sum = chgcars
+            .into_iter()
+            .reduce(|a, b| (a + b).expect("ChargeDensity addition failed"))
+            .ok_or_else(|| anyhow!("Failed to sum CHGCARs"))?;
 
-        info!("Writing combined charge density to {:?}", self.output);
-        chg_sum.to_file(&self.output)?;
+        info!("Writing summed charge density to {:?}", self.output);
+        sum.to_file(&self.output)?;
 
         Ok(())
     }
 }
-
